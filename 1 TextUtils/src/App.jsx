98997ea66d587a81fc5f7345ab24791e1a46fa4e
satorugojo3795 <sakshamjoshi129@gmail.com>
2024-05-16 17:15:41 +0100

    added files

---
+++ 1 TextUtils/src/App.jsx
@@ -0,0 +1,16 @@
+import About from './components/About'
+import Navbar from './components/Navbar'
+// import TextForm from './components/TextForm'
+function App() {
+  return (
+    <>
+      <Navbar title="Text Utils"/>
+      {/* <div className="container">
+        <TextForm heading="Enter text to analyse"/>
+      </div> */}
+      <About/>
+    </>
+  )
+}
+
+export default App
---
+++ 1 TextUtils/src/components/TextForm.jsx
@@ -0,0 +1,53 @@
+// import React from 'react'
+import { useState } from 'react'
+import PropTypes from 'prop-types'
+
+
+const TextForm = (props) => {
+
+    const handleUpClick = () =>{
+        console.log('Uppercase was clicked');
+        let newText = text.toUpperCase();
+        setText(newText);
+    }
+
+    const handleLoClick = () =>{
+        console.log('Lowercase was clicked');
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+
+    const handleOnChange = (event) =>{
+        setText(event.target.value);    
+    }
+
+    const handleOnClick = (event) =>{
+        if(event.target.value === "Enter Text Here"){
+            setText("")
+        }
+    }   
+    
+    const ClearText = () =>{
+        setText("");
+    }
+    const [text,setText] = useState("Enter Text Here");
+    return (
+        <>
+            <h1>{props.heading}</h1>
+            <div className="mb-3">
+                <textarea className="form-control" onChange={handleOnChange} value={text} id="exampleFormControlTextarea1" rows="8" onClick={handleOnClick}></textarea>
+            </div>
+            <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
+            <button className="btn btn-primary mx-3" onClick={handleLoClick}>Convert to LowerCase</button>
+            <button className="btn btn-primary mx-3" onClick={ClearText}>Clear Text</button>
+            <div className="container my-3">
+                <p>{text.split(" ").length} words and {text.length} characters</p>
+            </div>
+        </>
+    )
+}
+
+TextForm.propTypes = {
+    heading: PropTypes.string
+  };
+export default TextForm
