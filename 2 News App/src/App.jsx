e602077786af19d9a01eb0a905cb6cd31830fb20
Tanu Soni <tanusoni@Tanus-MacBook-Air.local>
2024-05-19 01:21:47 +0100

    updated files

--- 2 News App/src/App.jsx
+++ 2 News App/src/App.jsx
@@ -1,11 +1,13 @@
 // import './App.css'
-import  { Component } from 'react'
+import  React, { Component } from 'react'
 import Navbar from "./components/Navbar"
+import News from './components/News'
 export default class App extends Component {
   render() {
     return (
       <div>
         <Navbar></Navbar>
+        <News/>
       </div>
     )
   }
---
+++ 2 News App/src/components/NewsItem.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export default class extends Component {
+    
+    render() {
+        let {title,description,imgUrl,newsUrl} = this.props;
+        return (
+            <div>
+                <div className="my-3">
+                    <div className="card" style={{width: "18rem"}}>
+                        <img className="card-img-top" src={imgUrl} alt="Card image cap"/>
+                            <div className="card-body">
+                                <h5 className="card-title">{title}...</h5>
+                                <p className="card-text">{description}...</p>
+                                <a href={newsUrl} target="_blank" className="btn btn-primary">Read More</a>
+                            </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
